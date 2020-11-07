36ccd0bb4fdc144d97cfc60f9b64a7812a81bf62
Arun Chippada <arunch@microsoft.com>
2020-11-07 10:40:51 -0800

    networks

---
+++ nn/3layer.py
@@ -0,0 +1,7 @@
+import mnist_loader as mnist
+import network
+
+training, validation, test = mnist.load_data_wrapper()
+network30 = network.Network([784, 30, 10])
+
+network30.SGD(training, 20, 10, 3, validation)
---
+++ nn/load_data_test.py
@@ -0,0 +1,7 @@
+import mnist_loader as mnist
+
+# data = mnist.load_data()
+data = mnist.load_data_wrapper()
+
+print(data)
+
