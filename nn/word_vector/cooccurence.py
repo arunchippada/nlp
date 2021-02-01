c69697766ee6fbcd7e759d2bc185655ff9d6d7f9
Arun Chippada <arunch@microsoft.com>
2021-02-01 09:39:30 -0800

    truncated_svd

--- nn/word_vector/cooccurence.py
+++ nn/word_vector/cooccurence.py
@@ -7,7 +7,6 @@ from nltk.corpus import reuters
 START_TOKEN = '<START>'
 END_TOKEN = '<END>'
 
-
 def read_corpus(category="crude"):
     """ Read files from the specified Reuter's category.
         Params:
@@ -19,19 +18,38 @@ def read_corpus(category="crude"):
     return [[START_TOKEN] + [w.lower() for w in reuters.words(f)] + [END_TOKEN] for f in files]
 
 
+def get_cooccurence(docs, w=4):
+    # distinct words
+    s = {w.lower() for w in [t for doc in docs for t in doc]}
+    tokens = sorted(list(s))
+    num_tokens = len(tokens)
+
+    cooccurennce = pd.DataFrame(np.zeros((num_tokens, num_tokens), dtype=int), index=tokens, columns=tokens)
+
+    for doc in docs:
+        docLength = len(doc)
+        for i in range(0, docLength):
+            vw = doc[i].lower()
+            for j in range(i - w, i + w + 1):
+                if i != j and 0 <= j < docLength:
+                    ow = doc[j].lower()
+                    cooccurennce.loc[vw][ow] = cooccurennce.loc[vw][ow] + 1
+
+    return cooccurennce
+
 if 0:
     docs = read_corpus()
     pprint.pprint(docs[:3], compact=True, width=100)
 
-# distinct words
-s = pd.Series([w.lower() for w in reuters.words(categories='crude')])
-tokens = [START_TOKEN] + list(s.value_counts().index) + [END_TOKEN]
-print(tokens)
+test_corpus = ["{} All that glitters isn't gold {}".format(START_TOKEN, END_TOKEN).split(" "), "{} All's well that ends well {}".format(START_TOKEN, END_TOKEN).split(" ")]
+test_cooccurence = get_cooccurence(test_corpus, 1)
+
+print(test_cooccurence)
+print(test_cooccurence.to_numpy())
+
+# print(get_cooccurence(read_corpus()))
 
 
-# cooccurennce = pd.DataFrame()
 
-# print(cooccurennce)
 
-# for d in read_corpus():
-#    for i in range(0, len(d) - 1):
+
---
+++ nn/word_vector/truncatedsvd.py
@@ -0,0 +1,96 @@
+import nltk
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from nltk.corpus import reuters
+from sklearn.decomposition import TruncatedSVD
+
+START_TOKEN = '<START>'
+END_TOKEN = '<END>'
+
+def read_corpus(category="crude"):
+    """ Read files from the specified Reuter's category.
+        Params:
+            category (string): category name
+        Return:
+            list of lists, with words from each of the processed files
+    """
+    files = reuters.fileids(category)
+    return [[START_TOKEN] + [w.lower() for w in reuters.words(f)] + [END_TOKEN] for f in files]
+
+
+def get_cooccurence(docs, w=4):
+    # distinct words
+    s = {w.lower() for w in [t for doc in docs for t in doc]}
+    tokens = sorted(list(s))
+    num_tokens = len(tokens)
+
+    cooccurennce = pd.DataFrame(np.zeros((num_tokens, num_tokens), dtype=int), index=tokens, columns=tokens)
+
+    for doc in docs:
+        docLength = len(doc)
+        for i in range(0, docLength):
+            vw = doc[i].lower()
+            for j in range(i - w, i + w + 1):
+                if i != j and 0 <= j < docLength:
+                    ow = doc[j].lower()
+                    cooccurennce.loc[vw][ow] = cooccurennce.loc[vw][ow] + 1
+
+    return cooccurennce
+
+
+def reduce_dim(df, k=2):
+    n_iters = 10     # Use this parameter in your call to `TruncatedSVD`
+    matrix = df.to_numpy()
+
+    print("Running Truncated SVD over %i words..." % (matrix.shape[0]))
+    svd = TruncatedSVD(n_components=k, n_iter=n_iters, random_state=42)
+    matrix_reduced = svd.fit_transform(matrix)
+    print("Done.")
+
+    reduced_df = pd.DataFrame(matrix_reduced, index=df.index, columns=['x', 'y'])
+    return reduced_df
+
+
+def plot_vectors(df, fig=1):
+    plt.figure(fig)
+    sns.scatterplot(x='x', y='y', data=df, label="Word vectors")
+    ax = plt.gca()
+    for w in df.index:
+        ax.text(df.loc[w]['x'], df.loc[w]['y'], w)
+
+
+cooccurence_from_csv = 1
+reduced_from_csv = 1
+co_csv_file = "E:\\scripts\\python\\nlp\\nn\\word_vector\\reuters_cooccurence.csv"
+red_csv_file = "E:\\scripts\\python\\nlp\\nn\\word_vector\\reuters_reduced.csv"
+
+test_corpus = ["{} The quick brown fox jumped over the lazy dog {}".format(START_TOKEN, END_TOKEN).split(" "),
+               "{} The quick yellow bobcat jumped over the wooden fence {}".format(START_TOKEN, END_TOKEN).split(
+                   " ")]
+
+if reduced_from_csv:
+    reduced = pd.read_csv(red_csv_file, index_col=0)
+else:
+    if cooccurence_from_csv:
+        cooccurence = pd.read_csv(co_csv_file, index_col=0)
+    else:
+        # cooccurence = get_cooccurence(test_corpus, 1)
+        cooccurence = get_cooccurence(read_corpus())
+        print("Writing cooccurence to csv")
+        cooccurence.to_csv(co_csv_file)
+
+    reduced = reduce_dim(cooccurence)
+    reduced.to_csv(red_csv_file)
+
+words = ['barrels', 'bpd', 'ecuador', 'energy', 'industry', 'kuwait', 'oil', 'output', 'petroleum', 'iraq']
+
+plot_vectors(reduced.loc[words])
+plt.show()
+
+
+
+
+
+
