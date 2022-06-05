d4a5278789f6e27b43a8e9c1cef3b94d99da6442
Arun Chippada <arunch@microsoft.com>
2022-06-05 11:38:04 -0700

    gpt

--- nn/torchsamples/torch_grad.py
+++ nn/torchsamples/torch_grad.py
@@ -9,6 +9,18 @@ x = torch.tensor([
     3
 ], requires_grad=True, dtype=float)
 
+z = 3 * x * x * x
+z.backward()
+
+pp.pprint(z)
+pp.pprint(x)
+pp.pprint(x.grad)
+
+# When y.backward is called below, it computes the gradient at x in the y path and then adds to the existing gradient
+# obtained from the z path. That is, gradient gets accumulated for x from multiple paths where it receives the gradient
+# if x.grad is set to none, then only gradient from y path will be in x.grad below
+# x.grad = None
+
 y = 2 * x * x
 # the y on RHS is treated as different variable, from the LHS y
 y = y * y
